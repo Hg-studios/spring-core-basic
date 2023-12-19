51c8096c068ba9e8f52820098548856a4786eab1
Hgstudio <tndms0308@naver.com>
2023-12-19 21:10:45 +0900

    refactor : AppConfig 리팩터링

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -1,6 +1,8 @@
 package hello.core;
 
+import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
@@ -8,11 +10,20 @@ import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
 
 public class AppConfig {
-    public MemberService memberService() {
-        return new MemberServiceImpl(new MemoryMemberRepository());
+    public MemberService memberService() { //MemberService 역할
+        return new MemberServiceImpl(memberRepository());
     }
 
-    public OrderService orderService() {
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+    public OrderService orderService() { //orderService 역할
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
+
+    private MemberRepository memberRepository() { //MemberRepository 역할
+        return new MemoryMemberRepository();
+    }
+
+    public DiscountPolicy discountPolicy() { //discountPolicy 역할
+        return new FixDiscountPolicy();
+    }
+
 }
