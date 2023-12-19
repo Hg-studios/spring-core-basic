bb658ec8be61381ea836b18087ec37959c052134
Hgstudio <tndms0308@naver.com>
2023-12-19 20:58:08 +0900

    feat : AppConfig 구현 (관심사의 분리)

--- src/test/java/hello/core/member/MemberServiceTest.java
+++ src/test/java/hello/core/member/MemberServiceTest.java
@@ -1,10 +1,22 @@
 package hello.core.member;
 
+import hello.core.AppConfig;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class MemberServiceTest {
-    MemberService memberService = new MemberServiceImpl();
+    MemberService memberService;
+
+    /**
+     * Test 전에 매번 memberService를 세팅해줘야 하는 이유가 뭐지?
+     * 그냥 한번만 해놓으면 안되나?
+     */
+    @BeforeEach
+    public void beforeEach() {
+        AppConfig appConfig = new AppConfig();
+        memberService = appConfig.memberService();
+    }
 
     @Test
     void join(){
