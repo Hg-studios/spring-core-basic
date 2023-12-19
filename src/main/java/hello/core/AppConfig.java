d1f75e5e590f73f32bceca0e0a242ff43136a0d6
Hgstudio <tndms0308@naver.com>
2023-12-20 03:20:44 +0900

    feat : 스프링으로 전환 코드 구현

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -9,20 +9,29 @@ import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class AppConfig {
+
+    @Bean
+    //스프링 컨테이너에 등록이 됨
     public MemberService memberService() { //MemberService 역할
         return new MemberServiceImpl(memberRepository());
     }
 
+    @Bean
     public OrderService orderService() { //orderService 역할
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
-    private MemberRepository memberRepository() { //MemberRepository 역할
+    @Bean
+    public MemberRepository memberRepository() { //MemberRepository 역할
         return new MemoryMemberRepository();
     }
 
+    @Bean
     public DiscountPolicy discountPolicy() { //discountPolicy 역할
 //        return new FixDiscountPolicy();
         return new RateDiscountPolicy();
